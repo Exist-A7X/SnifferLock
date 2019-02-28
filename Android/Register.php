638d3eb7a53180f20dcb63c398c65aa4cd880bcd
Abdulhamid <hamidbg95@gmail.com>
2019-02-28 22:00:11 +0100

    deleting passwords

--- Android/Register.php
+++ Android/Register.php
@@ -1,6 +1,6 @@
 <?php
-    $con = mysqli_connect("snifferlock.000webhostapp.com", "id8780537_userips", "bIrB71U7gFfv", "id8780537_userips");
-    
+    $con = mysqli_connect("my_host", "my_user", "my_password", "my_database");
+
     $name = $_POST["name"];
     $age = $_POST["age"];
     $username = $_POST["username"];
@@ -9,9 +9,9 @@
     $statement = mysqli_prepare($con, "INSERT INTO user (name, username, age, password) VALUES (?, ?, ?, ?)");
     mysqli_stmt_bind_param($statement, "siss", $name, $username, $age, $password);
     mysqli_stmt_execute($statement);
-    
+
     $response = array();
-    $response["success"] = true;  
-    
+    $response["success"] = true;
+
     echo json_encode($response);
 ?>
